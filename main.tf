85337f1694e672973dead4a233e146891c769b1e
nehapatil-coditas <neha.patil@coditas.com>
2024-07-16 14:42:37 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+ required_providers {
+   aws = {
+      source = "hashicorp/aws"
+      version = "2.43.0"
+    }
+ }
+ 
+ #Adding a backend
+ backend "s3" {
+   bucket  = "terraform-state-bucket-101"
+   region = "us-east-1"
+   key    = "terraform.tfstate"
+   encrypt = true
+ }
+}
+ 
+# Adding provider
+provider "aws" {
+ region = "us-east-1"
+}
+ 
+# Creating a Amazon Linux 2 VM
+resource "aws_instance" "test_instance" {
+ ami           = "ami-01fccab91b456acc2"
+ instance_type = "t2.nano"
+ tags = {
+   Name = "test_instance"
+ }
+}
